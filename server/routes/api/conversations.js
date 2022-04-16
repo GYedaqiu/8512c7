a027e581b2e5894e781bb8580a6410812246c01f
GYedaqiu <guanshan.ye@gmail.com>
2022-04-16 00:40:25 +0000

    remove the order option inside the include, this option has no effect only if separate:true option is used

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -21,7 +21,7 @@ router.get("/", async (req, res, next) => {
       attributes: ["id"],
       order: [[Message, "createdAt", "DESC"]],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        { model: Message},
         {
           model: User,
           as: "user1",
